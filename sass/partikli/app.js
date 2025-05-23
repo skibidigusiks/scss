640ff3308725e04cbebcca6bc13398e3fab81987
skibidigusiks <bujansgustavs@gmail.com>
2025-05-23 09:28:29 +0000

    piektdienas izmainas

--- sass/partikli/app.js
+++ sass/partikli/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 160,
         "density": {
           "enable": true,
           "value_area": 800
@@ -109,7 +109,7 @@ particlesJS('particles-js',
           "speed": 3
         },
         "repulse": {
-          "distance": 200
+          "distance": 100
         },
         "push": {
           "particles_nb": 4
